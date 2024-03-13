10baf9dff6df47afd2ff1bf52dce58f0b0feba2c
Nerots <daniuyan71@gmail.com>
2024-03-13 19:23:29 +0700

    add original one

--- day-2/ts/index.ts
+++ day-2/ts/index.ts
@@ -1,3 +1,22 @@
-const countBits = (num: number) => num.toString(2).split("0").join("").length;
+function convertBinary(num: number) {
+  let bits = "";
 
-console.log(countBits(1234));
+  while (num > 0) {
+    bits = (num % 2) + bits;
+    num = Math.floor(num / 2);
+  }
+
+  return bits;
+}
+
+function countBits(num: number) {
+  const binary = convertBinary(num);
+  let bitOne = 0;
+
+  binary.split("").map((bit) => (parseInt(bit) === 1 ? bitOne++ : 0));
+
+  return bitOne;
+}
+
+// one liner
+const countBitsOneLiner = (num: number) => num.toString(2).split("0").join("").length;
